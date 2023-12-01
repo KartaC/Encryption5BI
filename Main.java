7e1d06c865afa16cdfbc3645e17f40374c2571d8
KartaC <thomas05.rc@gmail.com>
2023-12-01 14:40:17 +0000

    primo caricamento

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+    
+    System.out.println("Cifriamo il nostro testo e trasformiamolo in un testo segreto!");
+  }
+  
+  Matrice m = new Matrice("TPSIT");
+  Vigenere v = new Vigenere(0,12,0,12,m);
+  Thread t = new Thread(v);
+  t.start();
+
+  Vigenere v1 = new Vigenere(13,25,13,25,m);
+  Thread t1 = new Thread(v);
+}
